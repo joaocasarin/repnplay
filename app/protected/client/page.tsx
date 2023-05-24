9000b524ada797f2eb812030032d91880b947a80
Joao Vitor Casarin <jcasarin@apple.com>
2023-05-24 14:58:03 -0300

    fix 2

--- app/protected/client/page.tsx
+++ app/protected/client/page.tsx
@@ -6,6 +6,35 @@ const ClientProtectedPage = () => {
     const { data: session } = useSession();
 
     console.log(session);
+
+    function generateListFromSession() {
+        if (session) {
+            const { user } = session;
+
+            if (!user) {
+              return (
+                <p className='mt-4 text-red-500'>
+                  Problem with session, please try again
+                </p>
+              )
+            } else {
+              return (
+                <ul className='mt-4'>
+                  {
+                    Object.keys(user).map((key) => {
+                      return (
+                        <li key={key}>
+                          <strong>{key}: </strong>
+                          <span className='font-light'>{user[key as keyof typeof user]}</span>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              )
+            }
+        }
+    }
     
     return (
         <section className='py-24'>
@@ -15,15 +44,7 @@ const ClientProtectedPage = () => {
           protected page
         </h1>
         <h2 className='mt-4 font-medium'>You are logged in as:</h2>
-        <ul>
-          {session?.user && (
-            Object.keys(session.user).map((key) => (
-              <li key={key}>
-                <strong>{key}: </strong> {session.user[key]}
-              </li>
-            ))
-          )}
-        </ul>
+        { generateListFromSession() }
       </div>
     </section>
     );
